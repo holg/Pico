a8cf66660a147e73c301c9c90e9cfed4cabf5126
holger <h.trahe@relux.com>
2015-06-10 20:50:37 +0200

    [ADD] added rewritten pico_editor experimentally added Error Handling, special for Hosting, where Error Logs are pain

--- index.php
+++ index.php
@@ -8,6 +8,45 @@ define('PLUGINS_DIR', ROOT_DIR .'plugins/');
 define('THEMES_DIR', ROOT_DIR .'themes/');
 define('CACHE_DIR', LIB_DIR .'cache/');
 
-require_once(ROOT_DIR .'vendor/autoload.php');
-require_once(LIB_DIR .'pico.php');
+register_shutdown_function('errorHandler');
+register_shutdown_function( "fatal_handler" );
+
+function fatal_handler() {
+  $errfile = "unknown file";
+  $errstr  = "shutdown";
+  $errno   = E_CORE_ERROR;
+  $errline = 0;
+
+  $error = error_get_last();
+
+  if( $error !== NULL) {
+    print_r($error);
+  }
+}
+function errorHandler() { 
+    $error = error_get_last();
+    $type = $error['type'];
+    $message = $error['message'];
+    if ($type = 64 && !empty($message)) {
+        echo "
+            <strong>
+              <font color=\"red\">
+              Fatal error captured:
+              </font>
+            </strong>
+        ";
+        echo "<pre>";
+        print_r($error);
+        echo "</pre>";
+    }
+}
+require_once(LIB_DIR ."parsedown/Parsedown.php");
+#echo "1".LIB_DIR ."parsedown/Parsedown.php success\n<br />";
+require_once(LIB_DIR ."parsedown-extra/ParsedownExtra.php");
+require_once(LIB_DIR ."twig/lib/Twig/Autoloader.php");
+require_once(LIB_DIR ."pico.php");
+#echo "2".LIB_DIR ."pico.php success\n<br />";
+
+
 $pico = new Pico();
+?>
---
+++ plugins/pico_editor/pico_editor.php
@@ -0,0 +1,188 @@
+<?php
+
+/**
+ * Editor plugin for Pico
+ *
+ * @author Gilbert Pellegrom
+ * @link http://pico.dev7studios.com
+ * @license http://opensource.org/licenses/MIT
+ * @version 1.1
+ */
+class Pico_Editor {
+
+	private $is_admin;
+	private $is_logout;
+	private $plugin_path;
+	private $password;
+	
+	public function __construct()
+	{
+		$this->is_admin = false;
+		$this->is_logout = false;
+		$this->plugin_path = dirname(__FILE__);
+		$this->password = '';
+		session_start();
+		
+		if(file_exists($this->plugin_path .'/pico_editor_config.php')){
+			global $pico_editor_password;
+			include_once($this->plugin_path .'/pico_editor_config.php');
+			$this->password = $pico_editor_password;
+		}
+	}
+	
+	public function request_url(&$url)
+	{
+		// Are we looking for /admin?
+		if($url == 'admin') $this->is_admin = true;
+		if($url == 'admin/new') $this->do_new();
+		if($url == 'admin/open') $this->do_open();
+		if($url == 'admin/save') $this->do_save();
+		if($url == 'admin/delete') $this->do_delete();
+		if($url == 'admin/logout') $this->is_logout = true;
+	}
+	
+	public function before_render(&$twig_vars, &$twig)
+	{
+		if($this->is_logout){
+			session_destroy();
+			header('Location: '. $twig_vars['base_url'] .'/admin');
+			exit;
+		}
+		
+		if($this->is_admin){
+			header($_SERVER['SERVER_PROTOCOL'].' 200 OK'); // Override 404 header
+			$loader = new Twig_Loader_Filesystem($this->plugin_path);
+			$twig_editor = new Twig_Environment($loader, $twig_vars);
+			if(!$this->password){
+				$twig_vars['login_error'] = 'No password set for the Pico Editor.';
+				echo $twig_editor->render('login.html', $twig_vars); // Render login.html
+				exit;
+			}
+				
+			if(!isset($_SESSION['pico_logged_in']) || !$_SESSION['pico_logged_in']){
+				if(isset($_POST['password'])){
+					if(sha1($_POST['password']) == $this->password){
+						$_SESSION['pico_logged_in'] = true;
+					} else {
+						$twig_vars['login_error'] = 'Invalid password.';
+						echo $twig_editor->render('login.html', $twig_vars); // Render login.html
+						exit;
+					}
+				} else {
+					echo $twig_editor->render('login.html', $twig_vars); // Render login.html
+					exit;
+				}
+			}
+				
+			echo $twig_editor->render('editor.html', $twig_vars); // Render editor.html
+			exit; // Don't continue to render template
+		}
+	}
+	
+	private function do_new()
+	{
+		if(!isset($_SESSION['pico_logged_in']) || !$_SESSION['pico_logged_in']) die(json_encode(array('error' => 'Error: Unathorized')));
+		$title = isset($_POST['title']) && $_POST['title'] ? strip_tags($_POST['title']) : '';
+		$file = $this->slugify(basename($title));
+		if(!$file) die(json_encode(array('error' => 'Error: Invalid file name')));
+		
+		$error = '';
+		$file .= CONTENT_EXT;
+		$content = '/*
+Title: '. $title .'
+Author: 
+Date: '. date('Y/m/d') .'		
+*/';
+		if(file_exists(CONTENT_DIR . $file)){
+			$error = 'Error: A post already exists with this title';
+		} else {
+			file_put_contents(CONTENT_DIR . $file, $content);
+		}
+		
+		die(json_encode(array(
+			'title' => $title,
+			'content' => $content,
+			'file' => basename(str_replace(CONTENT_EXT, '', $file)),
+			'error' => $error
+		)));
+	}
+	
+    public function get_file()
+    {
+        if(!isset($_SESSION['pico_logged_in']) || !$_SESSION['pico_logged_in']) die(json_encode(array('error' => 'Error: Unathorized')));
+        $file_url = isset($_POST['file']) && $_POST['file'] ? $_POST['file'] : '';
+		if(substr($file_url, -1) == '/') {
+            $dir = substr($file_url,1, strlen($file_url)-1);
+            $file = 'index';
+        }else {
+            $dir = dirname($file_url).'/';
+            $file = basename($file_url);
+        }
+        $fileabspath = CONTENT_DIR . $dir .$file . CONTENT_EXT;
+        if($dir == '/') {
+            $dir  = '';
+        }
+        if($file == ""){
+            $file = 'index';
+            $dir = $file_url;
+            $fileabspath = CONTENT_DIR . $dir . $file . CONTENT_EXT;
+        }
+        $file .= CONTENT_EXT;
+
+        return array($file, $file_url, $dir, $fileabspath);
+    }
+	
+	private function do_open()
+	{
+		list($file, $file_url, $dir, $fileabspath) = $this->get_file();
+        if(!$file) die('Error 1: Invalid file');
+
+        if(file_exists($fileabspath)) die(file_get_contents($fileabspath));
+		else die('Error 2: Invalid file:'.$fileabspath);
+	}
+	
+	private function do_save()
+	{
+        list($file, $file_url, $dir, $fileabspath) = $this->get_file();
+		if(!$file) die('Error: Invalid file');
+		$content = isset($_POST['content']) && $_POST['content'] ? $_POST['content'] : '';
+		if(!$content) die('Error: Invalid content');
+		
+		file_put_contents($fileabspath, $content);
+		die($content);
+	}
+	
+	private function do_delete()
+	{
+        list($file, $file_url, $dir, $fileabspath) = $this->get_file();
+		if(!$file) die('Error: Invalid file');
+		
+		if(file_exists($fileabspath)) die(unlink($fileabspath));
+	}
+	
+	private function slugify($text)
+	{ 
+		// replace non letter or digits by -
+		$text = preg_replace('~[^\\pL\d]+~u', '-', $text);
+		
+		// trim
+		$text = trim($text, '-');
+		
+		// transliterate
+		$text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
+		
+		// lowercase
+		$text = strtolower($text);
+		
+		// remove unwanted characters
+		$text = preg_replace('~[^-\w]+~', '', $text);
+		
+		if (empty($text))
+		{
+		return 'n-a';
+		}
+		
+		return $text;
+	}
+	
+}
